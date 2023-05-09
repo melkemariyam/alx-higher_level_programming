ca04e6ba0a09993366e9cb7044721791e13a2bdf
melkemariyam <samulili.hunt@gmail.com>
2023-05-09 03:56:54 -0700

    re

--- 0x01-python-if_else_loops_functions/13-insert_numbeer.c
+++ 0x01-python-if_else_loops_functions/13-insert_numbeer.c
@@ -1,70 +1,52 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_node - malloc and insert node into sorted singly linked list
- * @head: pointer to head of linked list
- * @number: data for new node
- * Return: address of new node, or NULL if failed
+ * insert_node - inserts node into a sorted singly linked list
+ * @head: pointer to pointer to the start of the list
+ * @number: the int to be inserted with the new node
+ * Return: a pointer to the new node or NULL if failed
  */
-
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *tmp = NULL;
-	listint_t *new = NULL;
+	listint_t *compare, *new;
 
-	if (!head)
+	if (head == NULL)
 		return (NULL);
 
-	/* malloc new node */
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->n = number;
 	new->next = NULL;
 
-	/* if no linked list, insert node as the only member */
 	if (*head == NULL)
 	{
 		*head = new;
-		(*head)->next = NULL;
 		return (new);
 	}
-	/* if only one node in linked list, do comparision and insert */
-	if ((*head)->next == NULL)
+	if ((*head)->n > new->n)
 	{
-		if ((*head)->n < new->n)
-			(*head)->next = new;
-		else
-		{
-			new->next = *head;
-			*head = new;
-		}
+		new->next = *head;
+		*head = new;
 		return (new);
 	}
 
-	/* if lots of nodes in linked list, do comparision and insert */
-	tmp = *head;
-	while (tmp->next != NULL)
+	compare = *head;
+
+	while (compare->next != NULL)
 	{
-		/* if new node num is smaller than first node, insert */
-		if (new->n < tmp->n)
+		if (compare->next->n > new->n)
 		{
-			new->next = tmp;
-			*head = new;
+			new->next = compare->next;
+			compare->next = new;
 			return (new);
 		}
-		/* if new node num is the same as an existing node, insert */
-		/* compare previous node and next node, insert in between */
-		if (((new->n > tmp->n) && (new->n < (tmp->next)->n)) ||
-		    (new->n == tmp->n))
-		{
-			new->next = tmp->next;
-			tmp->next = new;
-			return (new);
-		}
-		tmp = tmp->next;
+		compare = compare->next;
 	}
-	/* if new node is greatest and never inserted, insert now */
-	tmp->next = new;
+
+	compare->next = new;
+	new->next = NULL;
 	return (new);
 }
